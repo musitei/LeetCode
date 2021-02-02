0d4c851b04936c20dfcbbf57108122b325f82352
musitei <847941490@qq.com>
2021-02-02 22:11:41 +0800

    subarraySum2h前缀和+哈希表解法

---
+++ subarraySum2.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap < Integer, Integer > mp = new HashMap < > ();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+            //当Map集合中有这个key时，就使用这个key值，如果没有就使用默认值defaultValue
+        }
+        return count;
+    }
+}
+
+
+
+
+
