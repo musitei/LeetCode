372b4a3f834441ab43d7fd31331b6c1b35cf085f
musitei <847941490@qq.com>
2021-02-04 22:46:25 +0800

    LRUCache.java 修改

--- LRUCache.java
+++ LRUCache.java
@@ -1,21 +1,20 @@
-class LRUCache extends LinkedHashMap<Integer, Integer>{
-    private int capacity;
-    
-    public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
-        this.capacity = capacity;
-    }
+ class LRUCache extends LinkedHashMap<Integer,Integer>{
+        private int capacity;
+        public LRUCache(int capacity){
+            super(capacity,0.75F,true);
+            //根据指定容量、装载因子和键值对保持顺序创建一个LinkedHashMap
+            this.capacity=capacity;
+        }
+        public int get(int key){
+            return super.getOrDefault(key,-1);
 
-    public int get(int key) {
-        return super.getOrDefault(key, -1);
+        }
+        public void put(int key,int value){
+            super.put(key,value);
+        }
+        protected boolean removeEldestEntry(Map.Entry<Integer,Integer> eldest){
+            return size()>capacity;
+            //判断是否大于容量 返回一个布尔值
+            
+        }
     }
-
-    public void put(int key, int value) {
-        super.put(key, value);
-    }
-
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity; 
-    }
-}
